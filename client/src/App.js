98c1fcde6ef8cf411dfe6215f1e31a4441b8f06c
hasancifitci96 <hasanhikmetciftci@gmail.com>
2021-06-18 11:06:10 +0300

    57 20

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,40 @@
 import "./App.css"
-// import Products from "./components/Products/Products"
-// import NavbarLink from "./components/Navbar/Navbar"
-
+import React, { useState, useEffect } from "react"
+import { commerce } from "./lib/commerce"
 import { Products, Navbar } from "./components"
 
 function App() {
+  const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list()
+
+    setProducts(data)
+  }
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
+
+  const handleAddCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity)
+
+    setCart(item.card)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+    fetchCart()
+  }, [])
+
+  console.log(products)
+  console.log(cart)
+
   return (
     <div className="App">
       <Navbar />
-      <Products />
+      <Products products={products} onAddToCart={handleAddCart} />
     </div>
   )
 }
--- client/src/components/Products/Product/Product.js
+++ client/src/components/Products/Product/Product.js
@@ -7,27 +7,38 @@ import {
   Typography,
   IconButton,
 } from "@material-ui/core"
-import { AddShoppingCart, CallMissedSharp } from "@material-ui/icons"
-import useStyles from "../styles"
+import { AddShoppingCart } from "@material-ui/icons"
+
+import useStyles from "./styles"
 
 const Product = ({ product }) => {
   const classes = useStyles()
+
   return (
-    <Card className={CallMissedSharp.root}>
-      <CardMedia className={classes.media} image="" title={product.name} />
+    <Card className={classes.root}>
+      <CardMedia
+        className={classes.media}
+        image={product.media.source}
+        title={product.name}
+      />
       <CardContent>
-        <div className={classes.CardContent}>
-          <Typography variant="h5" gutterBottom>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
-          <Typography variant="h5">{product.price}</Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            ${product.price.formatted}
+          </Typography>
         </div>
-        <Typography variant="body2" color="textSecondary">
-          {product.description}
-        </Typography>
+        <Typography
+          dangerouslySetInnerHTML={{ __html: product.description }}
+          variant="body2"
+          color="textSecondary"
+          component="p"
+        />
       </CardContent>
       <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton arial-label="Add to Cart">
+        <IconButton aria-label="Add to Cart">
           <AddShoppingCart />
         </IconButton>
       </CardActions>
